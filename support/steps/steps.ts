2b4d9142184d57f552a27fbdefa2afb7582ff439
davidgoss <david@davidgoss.co>
2021-02-03 14:15:12 +0000

    remove existing steps and feature

--- support/steps/steps.ts
+++ support/steps/steps.ts
@@ -2,20 +2,4 @@ import {Given, When, Then} from '@cucumber/cucumber';
 import {CustomWorld} from '../CustomWorld';
 import { expect } from 'chai';
 
-Given('I am on the Cucumber website',  async function(this: CustomWorld) {
-    await this.browser.navigate();
-});
 
-When('I go to the Gherkin Reference', async function(this: CustomWorld) {
-    const page = await this.browser.homePage;
-    await page.toggleNavigation();
-    await page.toggleDocsMenu();
-    await page.navigateTo('/docs/gherkin/');
-    await page.navigateTo('/docs/gherkin/reference/');
-});
-
-Then('I can read about {string}', async function(this: CustomWorld, title: string) {
-    const page = await this.browser.documentationPage;
-    const result = await page.hasSection(title);
-    expect(result).to.be.true;
-});
